2fc5766dbaf5665b4d7d74744f349f4db733b9cf
AndreaTerenziani <andrea.terenziani.at@gmail.com>
2020-12-08 23:56:36 +0100

    Day 4 solution complete (took me een beetje longer than expected, whoops)

--- Day 4/src/day4.kt
+++ Day 4/src/day4.kt
@@ -1,9 +1,8 @@
 import java.io.File
 
-fun partA() : Int {
+fun test(check : (String) -> Boolean) : Int {
     var output = 0
     var lastPassport = ""
-    val requiredFields = arrayOf("byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid")
 
     File("input.txt").forEachLine { line ->
         if (line.isNotBlank()) {
@@ -14,31 +13,63 @@ fun partA() : Int {
         }
         else {
             //check last passport
-            if (checkPassport(requiredFields.toMutableList(), lastPassport)) output++
-
+            if (check(lastPassport)) output++
             lastPassport = ""
         }
     }
-
     //there may not have been an empty line at the end of the file to trigger the final check
-    if (checkPassport(requiredFields.toMutableList(), lastPassport)) output++
+    if (check(lastPassport)) output++
 
     return output
 }
 
-fun checkPassport(fields: MutableList<String>, passport: String): Boolean {
-    //check last passport
-    println(passport)
-
-    val tokens = passport.split(Regex(":(#*[a-zA-Z0-9]*)(\\s|\$)"))
-            .filter { tok -> tok.isNotBlank() && tok != "cid" }
-
-    println(tokens)
-
-    tokens.forEach { tok -> fields.remove(tok) }
-    return fields.size == 0
-}
-
 fun main() {
-    println(partA())
+    val requiredFields = arrayOf("byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid")
+
+    val checkA = { passport: String ->
+        val fields = requiredFields.toMutableList()
+        passport.split(Regex(":(#*[a-zA-Z0-9]*)(\\s|\$)"))
+                .forEach { tok -> fields.remove(tok) }
+
+        (fields.size == 0)
+    }
+
+    val checkB = { passport: String ->
+        var passportGUD = true
+        val fields = requiredFields.toMutableList()
+        passport.split(" ")
+                .filter { tok -> tok.isNotBlank() && !Regex("cid:.*").matches(tok)}
+                .forEach { tok ->
+                    if (passportGUD) {
+                        val id = tok.split(":")[0]
+                        val value = tok.split(":")[1]
+
+                        var valueGUD = true
+
+                        when (id) {
+                            "byr" -> valueGUD = (value.toInt() in 1920..2002)
+                            "iyr" -> valueGUD = (value.toInt() in 2010..2020)
+                            "eyr" -> valueGUD = (value.toInt() in 2020..2030)
+                            "hcl" -> valueGUD = (Regex("#[a-f0-9]{6}").matches(value))
+                            "ecl" -> valueGUD = (value in arrayOf("amb", "blu", "brn", "gry", "grn", "hzl", "oth"))
+                            "pid" -> valueGUD = (Regex("[0-9]{9}").matches(value))
+                            "hgt" -> if (Regex("[0-9]+(cm|in)").matches(value)) {
+                                val v = value.substring(0, value.length-2).toInt()
+                                when (value.substring(value.length-2)) {
+                                    "cm" -> valueGUD = (v in 150..193)
+                                    "in" -> valueGUD = (v in 59..76)
+                                }
+                            } else valueGUD = false
+                        }
+
+                        passportGUD = passportGUD && fields.contains(id) && valueGUD
+                        fields.remove(id)
+                    }
+                }
+
+        passportGUD && (fields.size == 0)
+    }
+
+    println(test(checkA))
+    println(test(checkB))
 }
